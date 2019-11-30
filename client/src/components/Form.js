d6b6a541683e5fba1056521b93fe061e72c7775d
Alex Puello <apuello88@yahoo.com>
2019-11-30 12:33:36 -0500

    handleChange function being introduced to take in user inputs and get a response sent to the db then render to the page.

--- client/src/components/Form.js
+++ client/src/components/Form.js
@@ -4,12 +4,21 @@ import React from 'react';
     margin:"10px"
   };
 
+
+//   Target event and handleChange
+// handleChange = (e) => {
+
+
+// };
+
 class Form extends React.Component {
     render(){
         return(
         <div>
+            {/* Do I put this handleChange thang here? */}
+            {/* onChange={this.state.handleChange} */}
             <form>
-                <input name="book_name" placeholder="Book Title" style={inputStyles}></input> 
+                <input name="book_name" placeholder="Book Title" style={inputStyles} ></input> 
                 <br/>
                 <input name="author" placeholder="Author" style={inputStyles}></input>
             </form>
