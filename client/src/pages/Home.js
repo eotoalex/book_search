44e98729843172c654583ecfa4c8876fb59402bb
Alex Puello <apuello88@yahoo.com>
2019-12-03 11:04:38 -0500

    removed nav component references

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,12 +1,10 @@
 import React from 'react';
-import Nav from "../components/Nav";
 import JumboTron from "../components/JumboTron";
 
 class Home extends React.Component{
   render() {
     return (
       <div className="App">
-        <Nav/>
         <JumboTron/>
       </div>
     );
